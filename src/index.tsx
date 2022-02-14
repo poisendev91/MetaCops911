2139b06a970f6a4d090a2886571b88dda2657a2f
SUBHAM KUNWAR <68282784+whiterabbit-hub@users.noreply.github.com>
2022-02-14 21:13:50 +0530

    My first commitjvgs

--- src/index.tsx
+++ src/index.tsx
@@ -6,9 +6,8 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'font-awesome/css/font-awesome.min.css';
 
 ReactDOM.render(
-  <React.StrictMode>
     <App />
-  </React.StrictMode>,
+ ,
   document.getElementById('root'),
 );
 
